0f7f8e4917a2f58d9587d67b78f62696c9dcf832
Safyre Anderson <safyre@identitymind.com>
2017-04-16 22:08:52 -0700

    added bash script to retroactively add data through a list of dates. only works on OSX

---
+++ imwithdata/date_loop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+currentDateTs=$(date -j -f "%y.%m.%d" $1 "+%s")
+endDateTs=$(date -j -f "%y.%m.%d" $2 "+%s")
+offset=86400
+
+while [ "$currentDateTs" -le "$endDateTs" ]
+do
+     date=$(date -j -f "%s" $currentDateTs "+%y.%m.%d")
+     echo $date
+     python imwithdata/es_etl/es_etl.py --date $date
+     currentDateTs=$(($currentDateTs+$offset))
+done
